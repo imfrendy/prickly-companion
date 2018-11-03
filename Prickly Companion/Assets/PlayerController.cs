e82045aca6cfbbea16a4b5b1d02726ad6fe28664
imfrendy <chickenscratchcoder@gmail.com>
2018-11-02 21:03:02 -0700

    janky player controller

---
+++ Prickly Companion/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public const float playerSpeed = 50f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, 0, playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, 0, -playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Prickly Companion/Assets/SnapToCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToCharacter : MonoBehaviour {
+    Vector3 offset;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = offset + player.transform.position;
+	}
+}
