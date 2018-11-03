d5b95e77d64c58508e5b9cd378a7772b756b0cd4
imfrendy <chickenscratchcoder@gmail.com>
2018-11-02 20:11:52 -0700

    made scene, deleted because i didnt like it

---
+++ Prickly Companion/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public Text fpsCounter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Update UI
+        fpsCounter.text = "FPS: " + (1 / Time.deltaTime);
+	}
+}
